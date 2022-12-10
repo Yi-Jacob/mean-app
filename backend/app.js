af874c5a88a32fae2b5811269856bacff3315d39
Yi-Jacob <jakeyi00@gmail.com>
2022-12-09 16:30:42 -0800

    added backend auth'

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -15,9 +16,8 @@ mongoose
   .then(() => {
     console.log("Connected to database!");
   })
-  .catch((err) => {
+  .catch(() => {
     console.log("Connection failed!");
-    console.log(err)
   });
 
 app.use(bodyParser.json());
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -38,5 +38,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
